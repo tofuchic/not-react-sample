8871a1fa011541cec31b6a3e1ada6b67628d4aea
tofuchic <blue_savant_syndrome@yahoo.co.jp>
2021-05-15 18:24:32 +0900

    スパゲッティコードを整理

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
 import "./styles.css";
 
+const getNewIncompleteListRow = (inputText) => {
+  // li作成
+  const li = document.createElement("li");
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  li.appendChild(div);
+  // p作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  div.appendChild(p);
+  // button作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    onClickMoveFromIncompleteList2CompleteList(li);
+  });
+  div.appendChild(completeButton);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    onClickDeleteFromIncompleteList(li);
+  });
+  div.appendChild(deleteButton);
+  return li;
+};
+
+const getNewCompleteListRow = (inputText) => {
+  // li作成
+  const li = document.createElement("li");
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  li.appendChild(div);
+  // p作成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+  div.appendChild(p);
+  // button作成
+  const returnButton = document.createElement("button");
+  returnButton.innerText = "戻す";
+  returnButton.addEventListener("click", () => {
+    onClickReturnFromCompleteList2IncompleteList(li);
+  });
+  div.appendChild(returnButton);
+  return li;
+};
+
 const onClickAdd = () => {
   // テキストボックスの値を取得し、空にする
   const inputText = document.getElementById("add-text").value;
@@ -7,49 +55,35 @@ const onClickAdd = () => {
     document.getElementById("add-text").value = "";
 
     // li作成
-    const li = document.createElement("li");
-    document.getElementById("incomplete-list").appendChild(li);
-
-    // div作成
-    const div = document.createElement("div");
-    div.className = "list-row";
-    li.appendChild(div);
-
-    // p作成
-    const p = document.createElement("p");
-    p.innerText = inputText;
-    div.appendChild(p);
-
-    // button作成
-    const completeButton = document.createElement("button");
-    completeButton.innerText = "完了";
-    completeButton.addEventListener("click", () => {
-      const moveTarget = completeButton.parentNode.parentNode;
-      moveTarget.childNodes[0].childNodes[1].remove();
-      moveTarget.childNodes[0].childNodes[1].remove();
-      const returnButton = document.createElement("button");
-      returnButton.innerText = "戻す";
-      returnButton.addEventListener("click", () => {
-        const moveTarget = returnButton.parentNode.parentNode;
-        moveTarget.childNodes[0].childNodes[1].remove();
-        moveTarget.childNodes[0].appendChild(completeButton);
-        moveTarget.childNodes[0].appendChild(deleteButton);
-        document.getElementById("incomplete-list").appendChild(moveTarget);
-      });
-      moveTarget.childNodes[0].appendChild(returnButton);
-      document.getElementById("complete-list").appendChild(moveTarget);
-      // appendの後にremoveする必要はない
-      // document.getElementById("incomplete-list").removeChild(moveTarget);
-    });
-    div.appendChild(completeButton);
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "削除";
-    deleteButton.addEventListener("click", () => {
-      const deleteTarget = deleteButton.parentNode.parentNode;
-      document.getElementById("incomplete-list").removeChild(deleteTarget);
-    });
-    div.appendChild(deleteButton);
+    document
+      .getElementById("incomplete-list")
+      .appendChild(getNewIncompleteListRow(inputText));
   }
+};
+
+const onClickDeleteFromIncompleteList = (deleteTarget) => {
+  document.getElementById("incomplete-list").removeChild(deleteTarget);
+  deleteTarget = null;
+};
+
+const onClickMoveFromIncompleteList2CompleteList = (moveTarget) => {
+  document
+    .getElementById("complete-list")
+    .appendChild(
+      getNewCompleteListRow(moveTarget.childNodes[0].childNodes[0].innerText)
+    );
+  document.getElementById("incomplete-list").removeChild(moveTarget);
+  moveTarget = null;
+};
+
+const onClickReturnFromCompleteList2IncompleteList = (moveTarget) => {
+  document
+    .getElementById("incomplete-list")
+    .appendChild(
+      getNewIncompleteListRow(moveTarget.childNodes[0].childNodes[0].innerText)
+    );
+  document.getElementById("complete-list").removeChild(moveTarget);
+  moveTarget = null;
 };
 
 document
